8d17b81b8a763a707c555903f1841419b5a68e68
GoldenLeon <agomez3@gustavus.edu>
2018-04-27 16:47:16 -0500

    Differentiated CourseListActivity and ScheduleListActivity, because they initially raised issues.

--- app/src/main/java/com/bignerdranch/android/gustavusplanner/CourseListActivity.java
+++ app/src/main/java/com/bignerdranch/android/gustavusplanner/CourseListActivity.java
@@ -2,7 +2,8 @@ package com.bignerdranch.android.gustavusplanner;
 
 import android.support.v4.app.Fragment;
 
-public class CourseListActivity extends SingleFragmentActivity{
+public class CourseListActivity extends SingleFragmentActivity {
+
     @Override
     protected Fragment createFragment() {
         new GetCourseInfo().execute("https://gustavus.edu/registrar/webadvisor/mstrfall.html");
--- app/src/main/java/com/bignerdranch/android/gustavusplanner/ScheduleListActivity.java
+++ app/src/main/java/com/bignerdranch/android/gustavusplanner/ScheduleListActivity.java
@@ -2,11 +2,9 @@ package com.bignerdranch.android.gustavusplanner;
 
 import android.support.v4.app.Fragment;
 
-public class CourseListActivity extends SingleFragmentActivity {
-
+public class ScheduleListActivity extends SingleFragmentActivity{
     @Override
     protected Fragment createFragment() {
-        new GetCourseInfo().execute("https://gustavus.edu/registrar/webadvisor/mstrfall.html");
-        return new CourseListFragment();
+        return new ScheduleListFragment();
     }
 }
