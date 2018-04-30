7063148834c38c21fa4d2c1206e076a30e3c37bf
Jack Reichenbach <jreichen@gustavus.edu>
2018-04-30 13:10:12 -0500

    Toolbar for ScheduleListFragment

--- app/src/main/java/com/bignerdranch/android/gustavusplanner/ScheduleListFragment.java
+++ app/src/main/java/com/bignerdranch/android/gustavusplanner/ScheduleListFragment.java
@@ -5,6 +5,8 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -14,6 +16,12 @@ public class ScheduleListFragment extends Fragment {
 
     private RecyclerView mScheduleRecyclerView;
     private ScheduleAdapter mAdapter;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -26,6 +34,12 @@ public class ScheduleListFragment extends Fragment {
         updateUI();
 
         return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_schedule_list, menu);
     }
 
     private void updateUI() {
