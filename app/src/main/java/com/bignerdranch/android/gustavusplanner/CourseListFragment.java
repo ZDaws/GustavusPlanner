03e4b8e1c7f742228006ec2af655e168665cc671
ZDaws <zach.a.dawson@gmail.com>
2018-05-07 12:29:07 -0500

    Added onCreate for CourseListFragment so that the menu options are displayed using setHasOptionsMenu(true)

--- app/src/main/java/com/bignerdranch/android/gustavusplanner/CourseListFragment.java
+++ app/src/main/java/com/bignerdranch/android/gustavusplanner/CourseListFragment.java
@@ -25,6 +25,12 @@ public class CourseListFragment extends Fragment {
     private TextView mTitleTextView;
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_schedule_list, container, false);
